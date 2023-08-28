0fa00216d4bbd3732c8921482e6d7880af032cbd
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2023-08-28 20:31:58 +0000

    adding routing

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,6 +7,7 @@ import { PiChatsDuotone } from "react-icons/pi";
 import { GoQuestion } from "react-icons/go";
 import { CgLogOut } from "react-icons/cg";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const menuItems = [
   {
@@ -45,6 +46,12 @@ function Sidebar() {
   const [selectedMenuItem, setSelectedMenuItem] = React.useState(
     menuItems[0].id
   );
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleNavigate = (item) => () => {
+    router.push(item.path);
+  }
 
   return (
     <div>
@@ -64,14 +71,23 @@ function Sidebar() {
         aria-label="Sidenav"
       >
         <div className="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+          {/* banner */}
+          <div className="flex items-center justify-between mb-6">
+            <div className="flex items-center">
+              <h1>
+                <span className="text-2xl font-bold text-gray-800 dark:text-white">
+                  MathMentor
+                </span>
+              </h1>
+            </div>
+          </div>
           <ul className="space-y-2">
             {menuItems.map((item) => (
-              <li key={item.id} onClick={() => setSelectedMenuItem(item.id)}>
-                <Link
-                  href={item.path}
+              <li key={item.id} onClick={handleNavigate(item)}>
+                <label
                   className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
                     ${
-                      selectedMenuItem === item.id
+                      pathname.includes(item.id)
                         ? "bg-gray-100 dark:bg-gray-700"
                         : ""
                     }
@@ -79,103 +95,10 @@ function Sidebar() {
                 >
                   {item.icon}
                   <span className="ml-3">{item.label}</span>
-                </Link>
+                </label>
               </li>
             ))}
           </ul>
-          <ul className="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-              >
-                <span className="ml-3">Docs</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-              >
-                <span className="ml-3">Components</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-              >
-                <span className="ml-3">Help</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20 border-r border-gray-200 dark:border-gray-700">
-          <a
-            href="#"
-            className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
-          ></a>
-          <a
-            href="#"
-            data-tooltip-target="tooltip-settings"
-            className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
-          ></a>
-          <div
-            id="tooltip-settings"
-            role="tooltip"
-            className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
-          >
-            Settings page
-            <div className="tooltip-arrow" data-popper-arrow></div>
-          </div>
-          <button
-            type="button"
-            data-dropdown-toggle="language-dropdown"
-            className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:hover:text-white dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600"
-          ></button>
-          <div
-            className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-            id="language-dropdown"
-          >
-            <ul className="py-1" role="none">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <div className="inline-flex items-center">English (US)</div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <div className="inline-flex items-center">Deutsch</div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <div className="inline-flex items-center">Italiano</div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <div className="inline-flex items-center">中文 (繁體)</div>
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </aside>
     </div>
